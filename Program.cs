f9f87956bc8eba87fb10a2d2c124caa9d249deea
AMANDA BOTELHO DE MORAES <41346828865@jpmorgan.sp>
2017-10-26 21:55:05 -0200

    excel

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NetOffice.ExcelApi;
+
+namespace excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CriarExcel();
+            //LerExcel();
+        }
+
+        public static void CriarExcel(){
+            Application ex = new Application();
+            Workbook workbook = ex.Workbooks.Add();
+
+            Worksheet workSheet = workbook.Worksheets[1] as Worksheet;
+
+            workSheet.Name = "nome";
+/*
+            ex.Cells[1,1].Value = "Ford";
+            ex.Cells[1,2].Value = "Focus GHIA";
+            ex.Cells[1,3].Value = "2.0";
+*/
+            ex.ActiveWorkbook.SaveAs(@"C:\Users\41346828865\excel\lista.xls");
+            ex.Quit();
+        }
+
+         public static void LerExcel(){
+            Application ex = new Application();
+            ex.Workbooks.Open(@"C:\Users\41346828865\excel\lista.xls");
+
+            string modelo = ex.Cells[1,1].Value.ToString();
+            Console.WriteLine(modelo);
+
+            ex.Quit();
+        }
+    }
+}
